934a436c04682062d4e5a0b64db8221d40e35ca4
cristinagonzalez <cristinagonzalez@itop.es>
2019-03-31 22:05:45 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fw-preview ref="preview"></fw-preview>
+    <fw-lista :elements="elements"></fw-lista>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Lista from './components/Lista';
+import Preview from './components/Preview';
+import elements from './assets/datos.json';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    'fw-lista': Lista,
+    'fw-preview': Preview,
+  },
+  data() {
+    return {
+      elements
+    }
+  },
+
 }
 </script>
 
-<style>
+<style lang="postcss">
+   
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+:root {
+  height: 100vh;
+  --bgcolor: #ddd;
+  --textcolor: #fff;
+  background: #222;
+  
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 4em;
+  display: flex;
+  justify-content: center;
+  max-width: 900px;
+  margin: 4em auto;
 }
 </style>
